bf1815c43426863faac714a40484981411feedf5
lakhvir kumar <lakhvirkumar998@gmail.com>
2025-11-13 19:05:48 +0530

    Added environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client';
 import '@fontsource-variable/inter';
 import './index.css';
 import App from './App.tsx';
-// import ReactGA from 'react-ga4';
-
-// ReactGA.initialize('G-111');
-// ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+import ReactGA from 'react-ga4';
+const gaId = import.meta.env.VITE_GA_ID;
+ReactGA.initialize(gaId);
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
