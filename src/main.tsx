a4b1b205027390633e39130dbfa0e63b7659151e
lakhvir kumar <lakhvirkumar998@gmail.com>
2025-11-13 18:51:05 +0530

    Removed slash from homepage

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import '@fontsource-variable/inter';
 import './index.css';
 import App from './App.tsx';
+// import ReactGA from 'react-ga4';
+
+// ReactGA.initialize('G-111');
+// ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
